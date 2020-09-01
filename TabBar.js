f2c771ba7ea7eb9ea0b8b9a9d9afa7aec70eee4c
Sverre <sverre.westerlund@happypancake.com>
2020-09-01 14:15:10 +0200

    Another tab bar style

--- TabBar.js
+++ TabBar.js
@@ -14,6 +14,7 @@ import Layout from './Layout';
 export default class TabBar extends React.Component {
   static propTypes = {
     ...Animated.View.propTypes,
+    tabBarStyle: ViewPropTypes.style,
     shadowStyle: ViewPropTypes.style,
   };
 
