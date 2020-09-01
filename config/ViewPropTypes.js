747aa89fc65becf1dea2c1185b3f44d2af66bb15
Sverre <sverre.westerlund@happypancake.com>
2020-09-01 14:38:34 +0200

    Another one

--- config/ViewPropTypes.js
+++ config/ViewPropTypes.js
@@ -1,5 +1,6 @@
 import { View, ViewPropTypes as RNViewPropTypes } from 'react-native';
+import DeprecatedViewPropTypes from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedViewPropTypes';
 
-const ViewPropTypes = RNViewPropTypes || View.propTypes;
- 
+const ViewPropTypes = RNViewPropTypes || View.propTypes || DeprecatedViewPropTypes;
+
 export default ViewPropTypes;
